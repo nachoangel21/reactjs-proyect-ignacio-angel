a57001c34925c60d2c93e117824977d5c11c0ca1
nachooangel <nachooaangel111@gmail.com>
2022-08-30 17:47:42 -0300

    contador con boton agregado

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@
 import './App.css';
 import Navbar from './Componentes/NavBar/Navbar'
 import ItemListConteiner from './Componentes/ItemListConteiner/ItemListConteiner'
+import ItemCount from './Componentes/ItemCount/ItemCount';
+import {useState} from 'react';
 
 function App() {
+  const [numeroProductos, setNumeroProductos] = useState(0);
+  const agregar = (productos) => {
+    console.log("Productos agregados", productos)
+    setNumeroProductos(productos);
+  }
+
+  
   return (
     <div className="App">
         <header className="App-header">
           <Navbar></Navbar>
         </header>
+        <ItemCount stock={5} initial={1} onAdd={agregar}/>
         <ItemListConteiner greeting="Contador con boton"></ItemListConteiner>
     </div>
   );
--- src/Componentes/ItemCount/ItemCount.js
+++ src/Componentes/ItemCount/ItemCount.js
@@ -1,38 +1,36 @@
-import {useState,  useEffect} from 'react';
+import {useState} from 'react';
 
-export const ItemCount = ({initial, stock, onAdd}) => {
-    const [contador, setContador] = useState ({initial})
+const ItemCount = ({initial, stock, onAdd}) => {
+    const [contador, setContador] = useState (initial)
     
-    useEffect(() => {
-        console.log("1er renderizado")
-    })
-   
-
-    useEffect(() => {
-        console.log("2do renderizado")
-    })
 
     const sumarUno = () => {
-        setContador() 
-            if (contador < {stock}, 1++);
-            else (window.alert("No hay mas stock"));  
+        if (contador < stock){
+            setContador(contador + 1)
+        }
+        else (window.alert("No hay mas stock"));  
     }    
-
-    useEffect(() => {
-        console.log("3er renderizado");
-    })
+ 
     
     const restarUno = () => {
-        setContador()
-            if (contador > {initial}, 1--);
-            else (window.alert("No se puede tener menos de un producto"))
+        if (contador > initial){
+            setContador(contador - 1)
+        }
+        else (window.alert("No se puede tener menos de un producto"))
     }
     
     
         return(
         <div>
-            <button onClick={sumarUno}>+</button><span>{contador}</span><button onClick={restarUno}>-</button>
-            <p>Cantidad de productos</p>
+            <div>
+                <button onClick={sumarUno}>+</button><span>{contador}</span><button onClick={restarUno}>-</button>
+                <p>Cantidad de productos</p>
+            </div>
+            <div>
+                <button onClick={() => (onAdd(contador))}>Agregar al carrito</button>
+            </div>
         </div>
     )
 }
+
+export default ItemCount;
